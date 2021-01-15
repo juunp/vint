3c49868e2b44f10505ab3a023823b19960669663
Judith <jpivoteau@invivox.com>
2021-01-15 13:58:32 +0100

    first commit

--- client/index.js
+++ client/index.js
@@ -1,10 +1,12 @@
 const socketUrl = 'http://localhost:9000';
 
-let connectButton;
-let disconnectButton;
 let socket;
-let statusInput;
-let tokenInput;
+let nameInput;
+let listnames;
+let serverNames = {};
+let serverContributions = {};
+let names = [];
+let contributions = [];
 
 const connect = () => {
   let error = null;
@@ -15,13 +17,6 @@ const connect = () => {
 
   socket.on('connect', () => {
     console.log('Connected');
-    statusInput.value = 'Connected';
-    connectButton.disabled = true;
-    disconnectButton.disabled = false;
-
-    socket.emit('authentication', {
-      token: tokenInput.value,
-    });
   });
 
   socket.on('unauthorized', (reason) => {
@@ -32,11 +27,25 @@ const connect = () => {
     socket.disconnect();
   });
 
+  socket.on('names', (msg) => {
+    if (isDifferentFromKnown(serverNames, msg)) {
+      serverNames = msg;
+      names = getListOfNames(msg);
+      recreateListOfNames(names);
+      recreateListOfContributions(names, contributions, socket.id);
+    }
+  })
+
+  socket.on('contributions', (msg) => {
+    if (isDifferentFromKnown(serverContributions, msg)) {
+      serverContributions = msg;
+      contributions = getListOfContributions(msg);
+      recreateListOfContributions(names, contributions, socket.id);
+    }
+  })
+
   socket.on('disconnect', (reason) => {
     console.log(`Disconnected: ${error || reason}`);
-    statusInput.value = `Disconnected: ${error || reason}`;
-    connectButton.disabled = false;
-    disconnectButton.disabled = true;
     error = null;
   });
 
@@ -47,9 +56,95 @@ const disconnect = () => {
   socket.disconnect();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  connectButton = document.getElementById('connect');
-  disconnectButton = document.getElementById('disconnect');
-  statusInput = document.getElementById('status');
-  tokenInput = document.getElementById('token');
+const addName = () => {
+  const nameInput = document.getElementById('name');
+  socket.emit('name-add', nameInput.value);
+};
+
+const addContrib = () => {
+  const contribInput = document.getElementById('contribution');
+  socket.emit('contrib-add', contribInput.value);
+};
+
+const addVote = (contrib, name) => {
+    socket.emit('vote-add', {contrib: contrib, name: name});
+}
+
+const removeVote = (contrib, name) => {
+  socket.emit('vote-remove', {contrib: contrib, name: name});
+}
+
+const getListOfNames = (msg) => {
+    names = [];
+    for (const val in msg){
+      names.push(msg[val].name);
+    }
+    return names;
+}
+
+const recreateListOfNames = (names) => {
+  const listnames = document.getElementById('list-names');
+  while (listnames.firstChild) {
+    listnames.firstChild.remove();
+  }
+  for (const i in names) {
+    let li = document.createElement('li');
+    let nameText = document.createTextNode(names[i]);
+    li.append(nameText);
+    listnames.appendChild(li);
+  }
+}
+
+const getListOfContributions = (msg) => {
+  contributions = [];
+  for (const val in msg) {
+    contributions.push(msg[val]);
+  }
+  return contributions;
+}
+
+const recreateListOfContributions = (names, contributions, socketId) => {
+  const listContributions = document.getElementById('contributions');
+  while (listContributions.firstChild) {
+    listContributions.firstChild.remove();
+  }
+  for (const val in contributions) {
+    let votehtml = '';
+    let hasVoted = false;
+    if (contributions[val].hasVoted.includes(socketId)) {
+      hasVoted = true;
+    }
+    for(let i in names) {
+      let name = names[i];
+      let hasVotedFor = false;
+      if (hasVoted && contributions[val].hasVotedFor[name]?.includes(socketId)) {
+        hasVotedFor = true;
+      }
+      if (hasVoted) {
+        if (hasVotedFor) {
+          votehtml += `${name}: ${contributions[val].votes[name] || 0} <button type="submit" onclick="removeVote('${contributions[val].value}', '${name}')">-</button>`
+        } else {
+          votehtml += `${name}: ${contributions[val].votes[name] || 0}`;
+        }
+      } else {
+        votehtml += `${name}: ${contributions[val].votes[name] || 0} <button type="submit" onclick="addVote('${contributions[val].value}', '${name}')">+</button>`;
+      }
+    }
+    let html = `<li>
+      ${contributions[val].value}
+      ${votehtml}
+      </li>`
+    listContributions.innerHTML = html;
+  }
+}
+
+const isDifferentFromKnown = (a, b) => {
+  return JSON.stringify(a) !== JSON.stringify(b);
+}
+
+document.addEventListener('DOMContentLoaded', () => {  
+  connect();
 });
+
+//fullstack angular laravel
+//laravel + connaissance front
--- server/index.js
+++ server/index.js
@@ -1,84 +1,145 @@
+require('dotenv').config();
 const http = require('http');
 const io = require('socket.io')();
-const socketAuth = require('socketio-auth');
-const adapter = require('socket.io-redis');
-
-const redis = require('./redis');
+const MongoClient = require('mongodb').MongoClient;
 
 const PORT = process.env.PORT || 9000;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+const uriMongoDB = `mongodb+srv://${USERNAME}:${PASSWORD}@sandbox.1vyym.mongodb.net`;
+const database = 'voting_machine';
+const contribution_coll = 'contribution';
+const name_coll = 'name';
+
 const server = http.createServer();
 
-const redisAdapter = adapter({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || 6379,
-  password: process.env.REDIS_PASS || 'password',
+io.attach(server);
+  
+const names = {};
+const contributions = {};
+
+io.on('connection', (socket) => {
+  console.log(`Socket ${socket.id} connected.`);
+
+  socket.on('disconnect', () => {
+    console.log(`Socket ${socket.id} disconnected.`);
+  });
+
+  socket.on('name-add', async (msg) => {
+    await createOrUpdateName(socket.id, msg);
+    emit(socket);
+  });
+
+  socket.on('contrib-add', (msg) => {
+    createOrUpdateContribution(socket.id, msg);
+    if (contributions[socket.id]) {
+      delete contributions[socket.id];
+    }
+    contributions[socket.id] = {value : msg, votes: {}};
+    emit(socket);
+  });
+
+  socket.on('vote-add', (msg) => {
+    addVote(msg.contrib, msg.name, 1, socket.id);
+    emit(socket);
+  })
+
+  socket.on('vote-remove', (msg) => {
+    addVote(msg.contrib, msg.name, -1, socket.id);
+    emit(socket);
+  })
+
+  setInterval(async () => {
+    emitNames(socket);
+  }, 3000)
+
+  setInterval(async () => {
+    emitContributions(socket);
+  }, 3000)
+
 });
 
-io.attach(server);
-io.adapter(redisAdapter);
+function emit(socket) {
+  emitNames(socket);
+  emitContributions(socket);
+}
 
-// dummy user verification
-async function verifyUser (token) {
-  return new Promise((resolve, reject) => {
-    // setTimeout to mock a cache or database call
-    setTimeout(() => {
-      // this information should come from your cache or database
-      const users = [
-        {
-          id: 1,
-          name: 'mariotacke',
-          token: 'secret token',
-        },
-      ];
+async function emitNames(socket) {
+  socket.emit('names', await getNames());
+}
 
-      const user = users.find((user) => user.token === token);
+async function emitContributions(socket) {
+  socket.emit('contributions', await getContributions());
+}
 
-      if (!user) {
-        return reject('USER_NOT_FOUND');
+function getNames() {
+  return MongoClient.connect(uriMongoDB, {}).then((client) =>  {
+    return client.db(database).collection(name_coll).find().toArray().then((val) => {
+      return val;
+    })
+    .finally(() => {client.close();});
+  })
+}
+
+function getContributions() {
+  return MongoClient.connect(uriMongoDB, {}).then((client) =>  {
+    return client.db(database).collection(contribution_coll).find().toArray().then((val) => {
+      return val;
+    })
+    .finally(() => {client.close();});
+  })
+}
+
+function createOrUpdateName(socketId, name) {
+  MongoClient.connect(uriMongoDB, function(err, client) {
+    client.db(database).collection(name_coll).findOne({'socket-id': socketId}).then((val) => {
+      if (val == null) {
+        client.db(database).collection(name_coll).insertOne({'socket-id': socketId, 'name': name});
+      } else {
+        client.db(database).collection(name_coll).findOneAndUpdate({'socket-id': socketId}, {'$set': {'name': name}});
       }
-
-      return resolve(user);
-    }, 200);
+        }, (err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      client.close();
+    });
   });
 }
 
-socketAuth(io, {
-  authenticate: async (socket, data, callback) => {
-    const { token } = data;
-
-    try {
-      const user = await verifyUser(token);
-      const canConnect = await redis
-        .setAsync(`users:${user.id}`, socket.id, 'NX', 'EX', 30);
-
-      if (!canConnect) {
-        return callback({ message: 'ALREADY_LOGGED_IN' });
+function createOrUpdateContribution(socketId, contributionName) {
+  MongoClient.connect(uriMongoDB, function(err, client) {
+    client.db(database).collection(contribution_coll).findOne({'socket-id': socketId}).then((val) => {
+      if (val == null) {
+        client.db(database).collection(contribution_coll).insertOne({'socket-id': socketId, 'value': contributionName, 'hasVoted': [], 'votes': {}, 'hasVotedFor': {}});
+      } else {
+        client.db(database).collection(contribution_coll).findOneAndUpdate({'socket-id': socketId}, {'$set': {'value': contributionName}});
       }
+        }, (err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      client.close();
+    });
+  });
+}
 
-      socket.user = user;
-
-      return callback(null, true);
-    } catch (e) {
-      console.log(`Socket ${socket.id} unauthorized.`);
-      return callback({ message: 'UNAUTHORIZED' });
-    }
-  },
-  postAuthenticate: async (socket) => {
-    console.log(`Socket ${socket.id} authenticated.`);
-
-    socket.conn.on('packet', async (packet) => {
-      if (socket.auth && packet.type === 'ping') {
-        await redis.setAsync(`users:${socket.user.id}`, socket.id, 'XX', 'EX', 30);
+function addVote(contributionName, votedFor, $expr, socketId) {
+  let action = '$push';
+  if ($expr < 0) {
+    action = '$pull'
+  }
+  MongoClient.connect(uriMongoDB, function(err, client){
+    client.db(database).collection(contribution_coll).findOne({'value': contributionName, 'hasVoted': {'$in': [socketId]}}).then((val) => {
+      if ($expr > 0 && val === null) {
+        client.db(database).collection(contribution_coll).findOneAndUpdate({'value': contributionName}, {[`${action}`]: {[`hasVotedFor.${votedFor}`]: socketId, 'hasVoted': socketId}, '$inc': {[`votes.${votedFor}`]: $expr}});
+      } else if (val !== null) {
+        if (val.hasVotedFor[votedFor]?.includes(socketId)) {
+          client.db(database).collection(contribution_coll).findOneAndUpdate({'value': contributionName}, {[`${action}`]: {[`hasVotedFor.${votedFor}`]: socketId, 'hasVoted': socketId }, '$inc': {[`votes.${votedFor}`]: $expr}});
+        }
       }
     });
-  },
-  disconnect: async (socket) => {
-    console.log(`Socket ${socket.id} disconnected.`);
-
-    if (socket.user) {
-      await redis.delAsync(`users:${socket.user.id}`);
-    }
-  },
-})
+  });
+}
 
 server.listen(PORT);
